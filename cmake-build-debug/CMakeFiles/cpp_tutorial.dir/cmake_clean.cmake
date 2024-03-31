41cce203af966a18aef949831cf6470c469c2c03
nthr3ads <ebxeax@hotmail.com>
2024-03-31 23:51:03 +0800

    f

---
+++ cmake-build-debug/CMakeFiles/cpp_tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_tutorial.dir/cpp_tutorial.cpp.o"
+  "CMakeFiles/cpp_tutorial.dir/cpp_tutorial.cpp.o.d"
+  "cpp_tutorial"
+  "cpp_tutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
